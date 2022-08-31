c8975498297d3deaf4737e1ac686a6c23a5f495d
jhar3ll <53156293+jhar3ll@users.noreply.github.com>
2022-08-31 18:42:36 -0500

    removed initiate() method

--- bikeTracker.py
+++ bikeTracker.py
@@ -7,22 +7,18 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from tweetLogic import sendDM
 
-def initiate():
-    global driver
-    global soup
-    service = Service('C:\\Users\\kapti\\.vscode\Workspace\\building\\FE_Tracker\\chromedriver.exe')
-    options = Options()
-    option_args = ['--no-sandbox','--disable-dev-shm-usage','--disable-gpu',"--start-maximized","--user-data-dir=C:\\Users\\kapti\\PycharmProjects\\lbt\\pd3070"]
-    for arg in option_args:
-        options.add_argument(arg)
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get("https://www.polaris.com/en-us/account/orders/details/?orderId=375036")
-    soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
+service = Service('C:\\Users\\kapti\\.vscode\Workspace\\building\\FE_Tracker\\chromedriver.exe')
+options = Options()
+option_args = ['--no-sandbox','--disable-dev-shm-usage','--disable-gpu',"--start-maximized","--user-data-dir=C:\\Users\\kapti\\PycharmProjects\\lbt\\pd3070"]
+for arg in option_args:
+    options.add_argument(arg)
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(service=service, options=options)
+driver.get("https://www.polaris.com/en-us/account/orders/details/?orderId=375036")
+soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
     
 def getStatus():
     bikeStatuses = soup.find_all("li", {"class": "progress-bar-status__list-item"})
-   
     for i in bikeStatuses:
         if i.get("data-status-active") == 'True':
             currentBikeStatus = i.find("div").getText()
@@ -46,16 +42,16 @@ def getTime():
     return timestamp
 
 def runTracker():
-    initiate()
     status = True
-    lastPhase = "Scheduled"
-    lastDate = "Ship Date: 08/16/2022 - 08/31/2022"
+    lastPhase = "Built"
+    lastDate = "Ship Date: 09/16/2022"
     while status:
         currentStatus = getStatus()
         currentDate = getShipDate()
         print(f'{currentStatus} at {getTime()}. {currentDate}')
 
         if currentStatus != lastPhase or currentDate != lastDate:
+            print(f'Your Scout Rogue build status has changed to {currentStatus}, {currentDate}!')
             sendDM(f'Your Scout Rogue build status has changed to {currentStatus}, {currentDate}!', 135039188)
         time.sleep(1800)
         driver.refresh()
